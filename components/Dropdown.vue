bb4b66df11c83e4263bcaaa4b05fbafe01857ac2
Michaela Lehr <michi_lehr@web.de>
2019-05-02 20:15:19 +0200

    #5 REF move add-new dropdown and podcast panel into separate components ADD add-new dropdown

---
+++ components/Dropdown.vue
@@ -0,0 +1,44 @@
+<template>
+  <b-dropdown hoverable position="is-bottom-left" aria-role="list">
+    <button slot="trigger" class="button">
+      <span>Add new</span>
+      <b-icon icon="menu-down"></b-icon>
+    </button>
+    <b-dropdown-item aria-role="listitem">
+      <b-icon
+        class="r_dropdown__icon"
+        size="is-small"
+        icon="microphone"
+      ></b-icon>
+      <span> New Episode</span>
+    </b-dropdown-item>
+    <b-dropdown-item aria-role="listitem">
+      <span
+        class="has-background-dark r_dropdown__icon r_dropdown__podcast-cover"
+      ></span>
+      <span> New Podcast</span>
+    </b-dropdown-item>
+    <b-dropdown-item aria-role="listitem">
+      <span
+        class="has-background-dark r_dropdown__icon r_dropdown__network-cover"
+      ></span>
+      <span> New Network</span>
+    </b-dropdown-item>
+  </b-dropdown>
+</template>
+
+<style>
+.r_dropdown__icon {
+  display: inline-block;
+  margin-right: 10px;
+  vertical-align: text-bottom;
+  width: 14px;
+  height: 14px;
+}
+.r_dropdown__network-cover {
+  border-radius: 50%;
+}
+.r_dropdown__podcast-cover {
+  border-radius: 2px;
+}
+</style>
--- pages/networks.vue
+++ pages/networks.vue
@@ -2,66 +2,9 @@
   <section class="r_networks">
     <section class="r_networks__header">
       <h1 class="is-size-4">Hello, {{ username }}</h1>
-      <!-- TODO: add nav component for "Add new" button -->
-      <b-button @click.stop.prevent="createNetwork()">
-        Add new
-      </b-button>
+      <dropdown />
     </section>
-    <article class="r_podcast">
-      <header class="r_podcast__header has-background-light">
-        <div class="r_podcast__cover has-background-dark"></div>
-        <div class="r_podcast__title">
-          <h1 class="is-size-5">Logbuch:Netzpolitik</h1>
-          <h2 class="is-size-7">
-            Einblicke und Ausblicke in das netzpolitische Geschehen
-          </h2>
-        </div>
-        <p class="r_podcast__analytics">
-          <span class="is-size-4">38293</span>
-          <span class="is-size-7">Subscriptions</span>
-        </p>
-      </header>
-      <main class="r_podcast__main">
-        <ul class="r_podcast__episodes">
-          <li class="r_podcast__episodes__element">
-            <div class="r_podcast__episode-cover has-background-dark"></div>
-            <h3 class="r_podcast__episode-title">LNP293 Versteckt hintenrum</h3>
-            <p class="r_podcast__episode-info">
-              <span class="is-size-7">Downloads:</span>
-              <span> 23943</span>
-            </p>
-            <p class="r_podcast__episode-info">
-              <b-icon icon="check-circle" size="is-small"></b-icon>
-              26.10.2019
-            </p>
-            <button class="button r_podcast__episode-info">
-              <b-icon icon="open-in-new"></b-icon>
-            </button>
-            <button class="button">
-              <b-icon icon="dots-vertical"></b-icon>
-            </button>
-          </li>
-          <li class="r_podcast__episodes__element">
-            <div class="r_podcast__episode-cover has-background-dark"></div>
-            <h3 class="r_podcast__episode-title">LNP293 Versteckt hintenrum</h3>
-            <p class="r_podcast__episode-info">
-              <span class="is-size-7">Downloads:</span>
-              <span> 23943</span>
-            </p>
-            <p class="r_podcast__episode-info">
-              <b-icon icon="check-circle" size="is-small"></b-icon>
-              26.10.2019
-            </p>
-            <button class="button r_podcast__episode-info">
-              <b-icon icon="open-in-new"></b-icon>
-            </button>
-            <button class="button">
-              <b-icon icon="dots-vertical"></b-icon>
-            </button>
-          </li>
-        </ul>
-      </main>
-    </article>
+    <podcast />
   </section>
 </template>
 
@@ -71,54 +14,19 @@
   justify-content: space-between;
   margin-bottom: 40px;
 }
-.r_podcast {
-  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
-}
-.r_podcast__cover {
-  border-radius: 5px;
-  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
-  position: relative;
-  bottom: -20px;
-  margin-right: 10px;
-  width: 100px;
-  height: 100px;
-}
-.r_podcast__episodes__element {
-  display: flex;
-  align-items: center;
-  padding: 10px 0;
-}
-.r_podcast__episode-cover {
-  border-radius: 2px;
-  margin-right: 10px;
-  width: 30px;
-  height: 30px;
-}
-.r_podcast__episode-title {
-  flex-grow: 1;
-  margin-right: 20px;
-}
-.r_podcast__episode-info {
-  margin-right: 20px;
-}
-.r_podcast__header {
-  align-items: flex-end;
-  display: flex;
-  justify-content: space-between;
-  padding: 10px 20px;
-}
-.r_podcast__main {
-  padding: 20px 20px;
-}
-.r_podcast__title {
-  flex-grow: 1;
-}
 </style>
 
 <script>
 import { mapState } from 'vuex'
 
+import Dropdown from '~/components/Dropdown'
+import Podcast from '~/components/Podcast'
+
 export default {
+  components: {
+    Dropdown,
+    Podcast
+  },
   computed: mapState({
     isLoggedIn: state => state.auth.isLoggedIn,
     username: state => state.auth.username
