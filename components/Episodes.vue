426b974c20a17d4e4cc768b59470f62b959c0cbb
Michaela Lehr <michi_lehr@web.de>
2019-05-14 14:08:20 +0200

    #12 REF move episode list into separate component

---
+++ components/Episodes.vue
@@ -0,0 +1,147 @@
+<template>
+  <section class="has-background-white r_episodes">
+    <section class="r_episodes__header">
+      <b-input
+        class="r_episodes__header__search"
+        icon="magnify"
+        type="search"
+        placeholder="Search..."
+      ></b-input>
+      <b-field class="r_episodes__header__sortby" label="Sort by">
+        <b-select placeholder="Latest, alphabetically, ...">
+          <option value="1">Latest</option>
+          <option value="2">Alphabetically</option>
+        </b-select>
+      </b-field>
+      <b-button>
+        <b-icon size="is-small" icon="plus-circle-outline"></b-icon>
+        <span> Add new Episode</span>
+      </b-button>
+    </section>
+    <section class="r_episodes__main">
+      <ul v-if="podcast && podcast.episodes && podcast.episodes.length > 0">
+        <li
+          v-for="episode in podcast.episodes"
+          :key="episode.guid"
+          class="r_episodes__podcast"
+        >
+          <div class="r_episodes__podcast__main">
+            <div
+              class="r_episodes__main__cover has-background-light"
+              :style="{
+                backgroundImage: `url(${
+                  episode.image ? episode.image : podcast.image
+                })`
+              }"
+            ></div>
+            <p>{{ episode.title }}</p>
+          </div>
+          <p>
+            <b-icon size="is-small" icon="check-circle-outline"></b-icon>
+            <span> {{ episode.publishedAt }}</span>
+          </p>
+          <div class="r_episodes__podcast__aside">
+            <b-button type="is-text" outlined>
+              <b-icon icon="pencil"></b-icon>
+            </b-button>
+            <b-button type="is-text" outlined>
+              <b-icon icon="open-in-new"></b-icon>
+            </b-button>
+            <b-button type="is-text" outlined>
+              <b-icon icon="dots-vertical"></b-icon>
+            </b-button>
+          </div>
+        </li>
+      </ul>
+      <div class="r_episodes__footer">
+        <b-pagination
+          :total="total"
+          :current.sync="current"
+          :order="order"
+          :per-page="perPage"
+          aria-next-label="Next page"
+          aria-previous-label="Previous page"
+          aria-page-label="Page"
+          aria-current-label="Current page"
+        >
+        </b-pagination>
+      </div>
+    </section>
+  </section>
+</template>
+
+<style>
+.r_episodes {
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+}
+.r_episodes__header {
+  align-items: center;
+  border-bottom: 1px solid lightgray;
+  display: flex;
+  justify-content: space-between;
+  padding: 1.25rem;
+}
+.r_episodes__header__search {
+  flex-grow: 1;
+  margin-right: 1rem;
+}
+.r_episodes__header__sortby {
+  align-items: center;
+  display: flex;
+  margin-bottom: 0 !important;
+  margin-right: 1rem;
+}
+.r_episodes__header__sortby label {
+  font-weight: 400;
+  margin-bottom: 0 !important;
+  margin-right: 0.5rem;
+}
+.r_episodes__footer {
+  padding: 1rem;
+}
+.r_episodes__main__cover {
+  background-size: cover;
+  border-radius: 0.125rem;
+  margin-right: 0.5rem;
+  width: 1.875rem;
+  height: 1.875rem;
+}
+.r_episodes__podcast {
+  align-items: center;
+  border-bottom: 1px solid lightgray;
+  display: flex;
+  justify-content: space-between;
+  padding: 0.675rem 1.25rem;
+  transition: background-color 0.075s ease-in-out;
+}
+.r_episodes__podcast:hover {
+  background-color: lightgray;
+}
+.r_episodes__podcast__aside {
+  margin-left: 2rem;
+}
+.r_episodes__podcast__main {
+  align-items: center;
+  display: flex;
+  flex-grow: 1;
+}
+</style>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  data() {
+    return {
+      activeTab: 0,
+      total: 200,
+      current: 1,
+      perPage: 20,
+      order: 'is-centered'
+    }
+  },
+  computed: mapState({
+    podcast: state => state.podcasts.podcast
+  })
+}
+</script>
--- pages/podcasts/_id.vue
+++ pages/podcasts/_id.vue
@@ -14,116 +14,19 @@
             {{ podcast.subtitle }}
           </h1>
         </div>
-        <div class="r_podcast-hero__menu">
-          <b-button type="is-white" outlined>
-            <b-icon size="is-small" icon="settings"></b-icon>
-          </b-button>
-        </div>
       </div>
     </section>
-    <section class="has-background-white r_podcast-card">
-      <section class="r_podcast-card__header">
-        <h2 class="is-size-5">Episodes</h2>
-        <b-input icon="magnify" type="search" placeholder="Search..."></b-input>
-        <b-field class="r_podcast-card__header__sortby" label="Sort by">
-          <b-select placeholder="Latest, alphabetically, ...">
-            <option value="1">Latest</option>
-            <option value="2">Alphabetically</option>
-          </b-select>
-        </b-field>
-        <b-button>
-          <b-icon size="is-small" icon="plus-circle-outline"></b-icon>
-          <span> Add new Episode</span>
-        </b-button>
-      </section>
-      <section class="r_podcast-card__main">
-        <ul v-if="podcast && podcast.episodes && podcast.episodes.length > 0">
-          <li
-            v-for="episode in podcast.episodes"
-            :key="episode.guid"
-            class="r_podcast-card__podcast"
-          >
-            <div class="r_podcast-card__podcast__main">
-              <div
-                class="r_podcast-card__main__cover has-background-light"
-                :style="{
-                  backgroundImage: `url(${
-                    episode.image ? episode.image : podcast.image
-                  })`
-                }"
-              ></div>
-              <p>{{ episode.title }}</p>
-            </div>
-            <p>
-              <b-icon size="is-small" icon="check-circle-outline"></b-icon>
-              <span> {{ episode.publishedAt }}</span>
-            </p>
-            <div class="r_podcast-card__podcast__aside">
-              <b-button type="is-text" outlined>
-                <b-icon icon="pencil"></b-icon>
-              </b-button>
-              <b-button type="is-text" outlined>
-                <b-icon icon="open-in-new"></b-icon>
-              </b-button>
-              <b-button type="is-text" outlined>
-                <b-icon icon="dots-vertical"></b-icon>
-              </b-button>
-            </div>
-          </li>
-        </ul>
-      </section>
-    </section>
+    <b-tabs v-model="activeTab" class="r_podcast-tabs">
+      <b-tab-item label="Episodes">
+        <episodes :podcast="podcast"></episodes>
+      </b-tab-item>
+      <b-tab-item label="Analytics"></b-tab-item>
+      <b-tab-item label="Settings"></b-tab-item>
+    </b-tabs>
   </section>
 </template>
 
 <style>
-.r_podcast-card {
-  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
-  margin: 5rem 1.5rem;
-}
-.r_podcast-card__header {
-  align-items: center;
-  border-bottom: 1px solid grey;
-  display: flex;
-  justify-content: space-between;
-  padding: 1.25rem;
-}
-.r_podcast-card__header__sortby {
-  align-items: center;
-  display: flex;
-  margin-bottom: 0 !important;
-}
-.r_podcast-card__header__sortby label {
-  font-weight: 400;
-  margin-bottom: 0 !important;
-  margin-right: 0.5rem;
-}
-.r_podcast-card__main__cover {
-  background-size: cover;
-  border-radius: 0.125rem;
-  margin-right: 0.5rem;
-  width: 1.875rem;
-  height: 1.875rem;
-}
-.r_podcast-card__podcast {
-  align-items: center;
-  border-bottom: 1px solid lightgray;
-  display: flex;
-  justify-content: space-between;
-  padding: 0.675rem 1.25rem;
-  transition: background-color 0.075s ease-in-out;
-}
-.r_podcast-card__podcast:hover {
-  background-color: lightgray;
-}
-.r_podcast-card__podcast__aside {
-  margin-left: 2rem;
-}
-.r_podcast-card__podcast__main {
-  align-items: center;
-  display: flex;
-  flex-grow: 1;
-}
 .r_podcast-hero {
   padding-bottom: 2.5rem !important;
   padding-top: 11.25rem !important;
@@ -142,21 +45,25 @@
   width: 11.25rem;
   height: 11.25rem;
 }
-.r_podcast-hero__menu {
-  position: absolute;
-  top: 11.25rem;
-  right: 1.5rem;
-}
 .r_podcast-hero__title {
   font-weight: 400;
   margin-bottom: 0.5rem !important;
 }
+.r_podcast-tabs {
+  margin: 3.75rem 0.75rem;
+}
 </style>
 
 <script>
+import Episodes from '~/components/Episodes'
+
 export default {
+  components: {
+    Episodes
+  },
   data() {
     return {
+      activeTab: 0,
       podcast: null
     }
   },
