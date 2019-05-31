07e8166a9a9e85719d91236c13d4d61912889fc6
Letty <login@letty.io>
2019-05-31 17:55:05 +0200

    Test: pages/index.vue file tested #7

---
+++ test/pages_index.test.js
@@ -0,0 +1,27 @@
+import { shallowMount, createLocalVue } from '@vue/test-utils'
+import Buefy from 'buefy'
+import Index from '../pages/index'
+
+import Login from '../components/Login'
+import WikidataInput from '../components/WikidataInput'
+
+describe('pages-index.vue', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    const localVue = createLocalVue()
+    localVue.use(Buefy)
+
+    wrapper = shallowMount(Index, {
+      localVue
+    })
+  })
+
+  it('renders a vue instance', () => {
+    expect(shallowMount(Index).isVueInstance()).toBe(true)
+  })
+
+  it('check if login component exists', () => {
+    expect(wrapper.contains(Login)).toBe(true)
+  })
+})
