46b762e4b66059714ee3109fe6aa5cf1ebccf988
Letty <login@letty.io>
2019-06-04 18:30:23 +0200

    Test: test navigation actions #7

--- store/navigation.test.js
+++ store/navigation.test.js
@@ -1,4 +1,4 @@
-import { mutations } from './navigation'
+import { actions, mutations } from './navigation'
 
 describe('Test navigation mutations', () => {
   let state = {
@@ -31,5 +31,30 @@ describe('Test navigation mutations', () => {
     mutations.set_active_episode(state, 13)
 
     expect(state.activeEpisodeId).toEqual(13)
+  })
+})
+
+describe('Test navigation actions', () => {
+  it('Test active network', () => {
+    const data = { activeNetworkId: 42 }
+    const commit = jest.fn()
+
+    actions.setActiveNetwork({ commit }, data)
+    expect(commit).toHaveBeenCalledWith('set_active_network', data)
+  })
+
+  it('Test active episode', () => {
+    const data = { activeEpisodeId: 23 }
+    const commit = jest.fn()
+
+    actions.setActiveEpisode({ commit }, data)
+    expect(commit).toHaveBeenCalledWith('set_active_episode', data)
+  })
+  it('Test active podcast', () => {
+    const data = { activePodcastId: 13 }
+    const commit = jest.fn()
+
+    actions.setActivePodcast({ commit }, data)
+    expect(commit).toHaveBeenCalledWith('set_active_podcast', data)
   })
 })
