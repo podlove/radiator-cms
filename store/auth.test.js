1042a811711181a9694a31e8fda70443786beb7c
Letty <login@letty.io>
2019-06-04 16:57:47 +0200

    Test: test set session auth #7

---
+++ store/auth.test.js
@@ -0,0 +1,39 @@
+import { mutations } from './auth'
+
+describe('Test auth mutations', () => {
+  let state = {
+    isLoggedIn: null,
+    username: null,
+    token: null
+  }
+
+  const data = {
+    username: 'letty',
+    token: 'supergeheimestokendingsieinsÖLF'
+  }
+
+  it('Test reset session', () => {
+    mutations.set_session(state, data)
+
+    expect(state).toEqual({
+      isLoggedIn: true,
+      username: data.username,
+      token: data.token
+    })
+  })
+
+  // it('Test reset session', () => {
+  //   state = {
+  //     isLoggedIn: true,
+  //     ...data.data
+  //   }
+
+  //   mutations.reset_session(state)
+
+  //   expect(state).toEqual({
+  //     isLoggedIn: null,
+  //     username: null,
+  //     token: null
+  //   })
+  // })
+})
