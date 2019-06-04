4803daa41de8aee3815179c1e97383c2028d9a26
Letty <login@letty.io>
2019-06-04 15:43:58 +0200

    Chore: include store folder into code coverage #7

--- jest.config.js
+++ jest.config.js
@@ -12,7 +12,8 @@ module.exports = {
   collectCoverage: true,
   collectCoverageFrom: [
     '<rootDir>/components/**/*.vue',
-    '<rootDir>/pages/**/*.vue'
+    '<rootDir>/pages/**/*.vue',
+    '<rootDir>/store/**/*.js'
   ],
   setupFiles: ['<rootDir>/setupJest.js']
 }
