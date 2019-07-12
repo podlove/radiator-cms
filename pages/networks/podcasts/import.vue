3204c9ae19fd0adf0a08fdb560fbfe1698be4353
Letty <login@letty.io>
2019-07-12 16:28:42 +0200

    Feat: Episode import overview #60

--- pages/networks/podcasts/import.vue
+++ pages/networks/podcasts/import.vue
@@ -82,7 +82,72 @@
             </div>
           </section>
         </b-step-item>
-        <b-step-item label="Import Episodes"></b-step-item>
+        <b-step-item label="Import Episodes">
+          <section>
+            <div class="podcast-name">
+              <div
+                class="r_podcast__cover has-background-dark"
+                :style="{
+                  backgroundImage: `url(${
+                    podcast && podcast.image ? podcast.image : ''
+                  })`
+                }"
+              ></div>
+              <div class="r_podcast__title">
+                <h1 class="is-size-5">{{ podcast.title }}</h1>
+                <h2 class="is-size-7">{{ podcast.subtitle }}</h2>
+              </div>
+            </div>
+            <p>{{ episodes.loaded }} / {{ episodes.total }} Episodes loaded</p>
+            <p>LADEBALKENPLATZHALTER</p>
+
+            <b-table :data="podcastFeedEpisode" striped="true">
+              <template slot-scope="props">
+                <b-table-column field="id" label="ID" width="40" numeric>
+                  {{ props.row.id }}
+                </b-table-column>
+                <b-table-column field="title" label="Title">
+                  {{ props.row.title }}
+                </b-table-column>
+                <b-table-column
+                  field="mp3"
+                  label="mp3"
+                  :class="checkField(props.row.mp3)"
+                >
+                  {{ props.row.mp3 }}
+                </b-table-column>
+                <b-table-column
+                  field="m4a"
+                  label="m4a"
+                  :class="checkField(props.row.m4a)"
+                >
+                  {{ props.row.m4a }}
+                </b-table-column>
+                <b-table-column
+                  field="ogg"
+                  label="ogg"
+                  :class="checkField(props.row.ogg)"
+                >
+                  {{ props.row.ogg }}
+                </b-table-column>
+                <b-table-column
+                  field="transcripts"
+                  label="Transcripts"
+                  :class="checkField(props.row.transcripts)"
+                >
+                  {{ props.row.transcripts }}
+                </b-table-column>
+                <b-table-column
+                  field="chapter_marks"
+                  label="Chapter Marks"
+                  :class="checkField(props.row.chapter_marks)"
+                >
+                  {{ props.row.chapter_marks }}
+                </b-table-column>
+              </template>
+            </b-table>
+          </section>
+        </b-step-item>
       </b-steps>
     </section>
   </section>
@@ -93,7 +158,11 @@ import { mapState } from 'vuex'
 export default {
   data() {
     return {
-      activeStep: 0,
+      activeStep: 2,
+      episodes: {
+        loaded: 123,
+        total: 321
+      },
       importAudioFiles: false,
       importMedia: {
         mp3: false,
@@ -109,6 +178,98 @@ export default {
         title: 'Zeitsprung',
         url: 'https://www.zeitsprung.fm'
       },
+      podcastFeedEpisode: [
+        {
+          id: 197,
+          title:
+            'ZS197: Das kurzlebige Königreich Finnland – und ein deutscher Adliger auf dem Thron',
+          mp3: true,
+          m4a: false,
+          ogg: false,
+          transcripts: true,
+          chapter_marks: false
+        },
+        {
+          id: 197,
+          title:
+            'ZS197: Das kurzlebige Königreich Finnland – und ein deutscher Adliger auf dem Thron',
+          mp3: true,
+          m4a: false,
+          ogg: true,
+          transcripts: true,
+          chapter_marks: false
+        },
+        {
+          id: 197,
+          title:
+            'ZS197: Das kurzlebige Königreich Finnland – und ein deutscher Adliger auf dem Thron',
+          mp3: true,
+          m4a: false,
+          ogg: true,
+          transcripts: true,
+          chapter_marks: true
+        },
+        {
+          id: 197,
+          title:
+            'ZS197: Das kurzlebige Königreich Finnland – und ein deutscher Adliger auf dem Thron',
+          mp3: true,
+          m4a: false,
+          ogg: true,
+          transcripts: true,
+          chapter_marks: true
+        },
+        {
+          id: 197,
+          title:
+            'ZS197: Das kurzlebige Königreich Finnland – und ein deutscher Adliger auf dem Thron',
+          mp3: true,
+          m4a: true,
+          ogg: true,
+          transcripts: true,
+          chapter_marks: false
+        },
+        {
+          id: 197,
+          title:
+            'ZS197: Das kurzlebige Königreich Finnland – und ein deutscher Adliger auf dem Thron',
+          mp3: false,
+          m4a: false,
+          ogg: true,
+          transcripts: false,
+          chapter_marks: true
+        },
+        {
+          id: 197,
+          title:
+            'ZS197: Das kurzlebige Königreich Finnland – und ein deutscher Adliger auf dem Thron',
+          mp3: true,
+          m4a: true,
+          ogg: false,
+          transcripts: true,
+          chapter_marks: false
+        },
+        {
+          id: 197,
+          title:
+            'ZS197: Das kurzlebige Königreich Finnland – und ein deutscher Adliger auf dem Thron',
+          mp3: false,
+          m4a: true,
+          ogg: true,
+          transcripts: true,
+          chapter_marks: false
+        },
+        {
+          id: 197,
+          title:
+            'ZS197: Das kurzlebige Königreich Finnland – und ein deutscher Adliger auf dem Thron',
+          mp3: true,
+          m4a: false,
+          ogg: true,
+          transcripts: true,
+          chapter_marks: true
+        }
+      ],
       shortID: 'zeitsprung-zsfm-2342',
       url: 'https://www.zeitsprung.fm/feed/mp4/'
     }
@@ -116,7 +277,12 @@ export default {
   computed: mapState({
     networks: state => state.networks.networks,
     podcast: state => state.podcasts.podcast
-  })
+  }),
+  methods: {
+    checkField(value) {
+      return value ? 'is-success' : 'is-danger'
+    }
+  }
 }
 </script>
 
