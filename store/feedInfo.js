343ec7142b56ca5670a63de082efa6529fe29196
Letty <login@letty.io>
2019-07-16 19:28:16 +0200

    Feat: Variable and api request for podcast feeds #60

--- store/feedInfo.js
+++ store/feedInfo.js
@@ -1,17 +1,22 @@
-import feedInfo from '~/api/queries/feedinfo.gql'
+import { feedInfo, getFeeds } from '~/api/queries/feedinfo.gql'
 
 export const state = () => ({
-  feedInfo: {}
+  feedInfo: {},
+  feeds: {}
 })
 
 export const mutations = {
   set_feedInfo(store, data) {
     store.feedInfo = data
+  },
+  set_feeds(store, data) {
+    store.feeds = data
   }
 }
 
 export const getters = {
-  feedInfo: state => state.feedInfo
+  feedInfo: state => state.feedInfo,
+  feeds: state => state.feeds
 }
 
 export const actions = {
@@ -28,6 +33,23 @@ export const actions = {
         })
         .then(({ data }) => data.feedInfo)
       await commit('set_feedInfo', res)
+    } catch (e) {
+      throw Error(e)
+    }
+  },
+  fetchFeeds: async function fetchFeeds({ commit }, data) {
+    const client = this.app.apolloProvider.defaultClient
+
+    try {
+      const res = await client
+        .query({
+          query: getFeeds,
+          variables: {
+            url: data.url
+          }
+        })
+        .then(({ data }) => data.feedInfo.feeds)
+      await commit('set_feeds', res)
     } catch (e) {
       throw Error(e)
     }
