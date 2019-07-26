0226c8f3b729bb0f3662b63750c35c436d5d1927
Letty <login@letty.io>
2019-07-26 17:44:50 +0200

    Feat: Expand feed store for tasks when importing a new podcast #60

--- store/feedInfo.js
+++ store/feedInfo.js
@@ -1,8 +1,10 @@
 import { feedInfo, getFeeds } from '~/api/queries/feedinfo.gql'
+import restTasks from '~/api/rest/tasks'
 
 export const state = () => ({
   feedInfo: {},
-  feeds: {}
+  feeds: {},
+  currentTask: {}
 })
 
 export const mutations = {
@@ -11,12 +13,16 @@ export const mutations = {
   },
   set_feeds(store, data) {
     store.feeds = data
+  },
+  set_current_task(store, data) {
+    store.currentTask = data
   }
 }
 
 export const getters = {
   feedInfo: state => state.feedInfo,
-  feeds: state => state.feeds
+  feeds: state => state.feeds,
+  currentTask: state => state.currentTask
 }
 
 export const actions = {
@@ -50,6 +56,15 @@ export const actions = {
         })
         .then(({ data }) => data.feedInfo.feeds)
       await commit('set_feeds', res)
+    } catch (e) {
+      throw Error(e)
+    }
+  },
+  createTask: async function createTask({ dispatch, commit }, data) {
+    data.token = this.$apolloHelpers.getToken()
+    try {
+      const res = await restTasks.create(data).then(data => data && data.data)
+      await commit('set_current_task', res)
     } catch (e) {
       throw Error(e)
     }
