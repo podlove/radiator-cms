d2a48db392738cb93feb54a55bf4225827a5ce2d
Letty <login@letty.io>
2019-09-03 15:02:45 +0200

    Feat: Add Rest API endpoints for chapters convert chapters from files, create and get them #46

---
+++ api/rest/audio-chapters.js
@@ -0,0 +1,57 @@
+import axios from 'axios'
+
+export default {
+  convertChapters: data => {
+    const query = JSON.stringify({
+      data: data.file
+    })
+    return axios.post(
+      `${process.env.baseUrl}/api/rest/${
+        process.env.backendVersion
+      }/convert/chapters`,
+      query,
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: 'Bearer ' + data.token
+        }
+      }
+    )
+  },
+  createChapters: data => {
+    const query = JSON.stringify({
+      chapter: {
+        audio_id: data.chapter.audio_id,
+        start: data.chapter.start,
+        title: data.chapter.title,
+        link: data.chapter.link,
+        image: data.chapter.image
+      }
+    })
+    return axios.post(
+      `${process.env.baseUrl}/api/rest/${process.env.backendVersion}/audios/${
+        data.chapter.audio_id
+      }/chapters`,
+      query,
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: 'Bearer ' + data.token
+        }
+      }
+    )
+  },
+  getChapters: data => {
+    return axios.get(
+      `${process.env.baseUrl}/api/rest/${process.env.backendVersion}/audios/${
+        data.audio_id
+      }/chapters?format=json`,
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: 'Bearer ' + data.token
+        }
+      }
+    )
+  }
+}
