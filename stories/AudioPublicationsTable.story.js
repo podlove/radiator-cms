e148b13f077a454e0a279b9669168eb2c2713110
Letty <login@letty.io>
2019-10-31 17:40:15 +0100

    Refactor: Update storybook configs and add dummy data to component

--- stories/AudioPublicationsTable.story.js
+++ stories/AudioPublicationsTable.story.js
@@ -1,11 +1,30 @@
+import { storiesOf } from '@storybook/vue'
 import AudioPublicationsTable from '../components/AudioPublicationsTable'
 
-export default {
-  title: 'Design Sytem|Molecule/AudioPublicationsTable',
-  component: AudioPublicationsTable
+export const network = {
+  id: 1,
+  audioPublications: [
+    {
+      id: 1,
+      title: 'Example 1',
+      publishState: 'unpublished',
+      audio: {
+        id: 1,
+        image:
+          'http://localhost:9000/radiator/podcast/2/cover_original.jpg?v=63739563914'
+      }
+    }
+  ]
 }
 
-export const blank = () => ({
-  components: { AudioPublicationsTable },
-  template: '<audio-publications-table></audio-publications-table>'
-})
+storiesOf('Design Sytem|Molecule/AudioPublicationsTable', module).add(
+  'default',
+  () => {
+    return {
+      components: { AudioPublicationsTable },
+      template:
+        '<audio-publications-table :network="network"></audio-publications-table>',
+      data: () => ({ network })
+    }
+  }
+)
