00a4e85fce8fdfa139acc1c126e398797d3b2c55
Letty <login@letty.io>
2019-10-31 20:16:41 +0100

    Refactor(stories): Extend mock data

--- stories/AudioPublicationsTable.story.js
+++ stories/AudioPublicationsTable.story.js
@@ -13,6 +13,56 @@ export const network = {
         image:
           'http://localhost:9000/radiator/podcast/2/cover_original.jpg?v=63739563914'
       }
+    },
+    {
+      id: 2,
+      title: 'Example 2',
+      publishState: 'unpublished',
+      audio: {
+        id: 2,
+        image:
+          'http://localhost:9000/radiator/podcast/2/cover_original.jpg?v=63739563914'
+      }
+    },
+    {
+      id: 3,
+      title: 'Example 3',
+      publishState: 'unpublished',
+      audio: {
+        id: 3,
+        image:
+          'http://localhost:9000/radiator/podcast/2/cover_original.jpg?v=63739563914'
+      }
+    },
+    {
+      id: 4,
+      title: 'Example 4',
+      publishState: 'unpublished',
+      audio: {
+        id: 4,
+        image:
+          'http://localhost:9000/radiator/podcast/2/cover_original.jpg?v=63739563914'
+      }
+    },
+    {
+      id: 5,
+      title: 'Example 5',
+      publishState: 'unpublished',
+      audio: {
+        id: 5,
+        image:
+          'http://localhost:9000/radiator/podcast/2/cover_original.jpg?v=63739563914'
+      }
+    },
+    {
+      id: 6,
+      title: 'Example 6',
+      publishState: 'unpublished',
+      audio: {
+        id: 6,
+        image:
+          'http://localhost:9000/radiator/podcast/2/cover_original.jpg?v=63739563914'
+      }
     }
   ]
 }
