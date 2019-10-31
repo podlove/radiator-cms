c85dc313fb6c0c31d778cec9e94563229dd215e4
Letty <login@letty.io>
2019-10-31 20:46:01 +0100

    Refactor(stories): Add mock data (modal not visible)

--- stories/ContributorsTable.story.js
+++ stories/ContributorsTable.story.js
@@ -1,11 +1,68 @@
+import { storiesOf } from '@storybook/vue'
 import ContributorsTable from '../components/ContributorsTable'
 
-export default {
-  title: 'Design Sytem|Molecule/ContributorsTable',
-  component: ContributorsTable
-}
+export const contributors = [
+  {
+    person: {
+      name: 'Tim Pritlove',
+      displayName: 'Tim',
+      uri: 'https://tim.pritlove.org/',
+      image:
+        'https://logbuch-netzpolitik.de/wp-content/cache/podlove/d4/a975f973c6d6e64ee5fb0e07abe7ef/linus-neumann_50x50.jpg'
+    },
+    contributionRole: {
+      id: 2,
+      title: 'Katzenlord'
+    }
+  },
+  {
+    person: {
+      name: 'Linus Neumann',
+      displayName: 'Tim',
+      uri: 'https://tim.pritlove.org/',
+      image:
+        'https://logbuch-netzpolitik.de/wp-content/cache/podlove/d4/a975f973c6d6e64ee5fb0e07abe7ef/linus-neumann_50x50.jpg'
+    },
+    contributionRole: {
+      id: 2,
+      title: 'Katzenlord'
+    }
+  },
+  {
+    person: {
+      name: 'Thomas Lohninger',
+      displayName: 'Tim',
+      uri: 'https://tim.pritlove.org/',
+      image:
+        'https://logbuch-netzpolitik.de/wp-content/cache/podlove/d4/a975f973c6d6e64ee5fb0e07abe7ef/linus-neumann_50x50.jpg'
+    },
+    contributionRole: {
+      id: 2,
+      title: 'Katzenlord'
+    }
+  },
+  {
+    person: {
+      name: 'Julia Reda',
+      displayName: 'Tim',
+      uri: 'https://tim.pritlove.org/',
+      image:
+        'https://logbuch-netzpolitik.de/wp-content/cache/podlove/d4/a975f973c6d6e64ee5fb0e07abe7ef/linus-neumann_50x50.jpg'
+    },
+    contributionRole: {
+      id: 2,
+      title: 'Katzenlord'
+    }
+  }
+]
 
-export const blank = () => ({
-  components: { ContributorsTable },
-  template: '<contributors-table></contributors-table>'
-})
+storiesOf('Design Sytem|Molecule/ContributorsTable', module).add(
+  'default',
+  () => {
+    return {
+      components: { ContributorsTable },
+      template: '<contributors-table :contributors="contributors"></contributors-table>',
+      data: () => ({ contributors })
+    }
+  }
+)
--- stories/EditContributorModal.story.js
+++ stories/EditContributorModal.story.js
@@ -1,11 +1,35 @@
+import { storiesOf } from '@storybook/vue'
 import EditContributorModal from '../components/EditContributorModal'
 
-export default {
-  title: 'Design Sytem|Molecule/EditContributorModal',
-  component: EditContributorModal
+export const contributor = {
+  person: {
+    name: 'Linus Neumann',
+    displayName: 'Tim',
+    uri: 'https://tim.pritlove.org/',
+    image:
+      'https://logbuch-netzpolitik.de/wp-content/cache/podlove/d4/a975f973c6d6e64ee5fb0e07abe7ef/linus-neumann_50x50.jpg'
+  },
+  contributionRole: {
+    id: 2,
+    title: 'Katzenlord'
+  }
 }
 
-export const blank = () => ({
-  components: { EditContributorModal },
-  template: '<edit-contributor-modal></edit-contributor-modal>'
-})
+export const contributionRole = {
+  id: 2,
+  title: 'Katzenlord'
+}
+
+export const isActive = true
+
+storiesOf('Design Sytem|Molecule/EditContributorModal', module).add(
+  'default',
+  () => {
+    return {
+      components: { EditContributorModal },
+      template:
+        '<edit-contributor-modal :contributor="contributor" :contribution-roles="contributionRole" :is-active="isActive"></edit-contributor-modal>',
+      data: () => ({ contributor })
+    }
+  }
+)
