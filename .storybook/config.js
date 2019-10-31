77442b8cd6c4ed2e4f505deb7bd74b3393df91ed
Letty <login@letty.io>
2019-10-31 17:42:14 +0100

    Refactor: Update storybook configs

--- .storybook/config.js
+++ .storybook/config.js
@@ -2,16 +2,8 @@ import { configure } from '@storybook/vue'
 import Buefy from 'buefy'
 import Vue from 'vue'
 
+import '!style-loader!css-loader!sass-loader!../assets/css/main.scss'
+
 Vue.use(Buefy)
 
-// function requireAll (requireContext) {
-//   return requireContext.keys().map(requireContext)
-// }
-
-// function loadStories () {
-//   requireAll(require.context('..', true, /^((?![\\/]node_modules|vendor[\\/]).)*\.story\.js?$/))
-// }
-
-// configure(loadStories, module)
-
-configure(require.context('../stories', true, /\.story\.js$/), module);
+configure(require.context('../', true, /\.story\.js$/), module);
