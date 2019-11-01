b58d2f151974f0c05040263ecd61740dedaac40b
Letty <login@letty.io>
2019-11-01 08:55:18 +0100

    Refactor: Use vuex in storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,11 @@
 import { configure } from '@storybook/vue'
 import Buefy from 'buefy'
 import Vue from 'vue'
+import Vuex from 'vuex'
 
 import '!style-loader!css-loader!sass-loader!../assets/css/main.scss'
 
 Vue.use(Buefy)
+Vue.use(Vuex)
 
 configure(require.context('../', true, /\.story\.js$/), module);
