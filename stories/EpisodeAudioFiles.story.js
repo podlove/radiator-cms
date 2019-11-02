868b554e0d6e41be1194cec6ae7cad04a7bec0da
Letty <login@letty.io>
2019-11-02 10:58:21 +0100

    Refactor(stories): Add mock data

--- stories/EpisodeAudioFiles.story.js
+++ stories/EpisodeAudioFiles.story.js
@@ -1,11 +1,29 @@
+import { storiesOf } from '@storybook/vue'
 import EpisodeAudioFiles from '../components/EpisodeAudioFiles'
 
-export default {
-  title: 'Design Sytem|Molecule/EpisodeAudioFiles',
-  component: EpisodeAudioFiles
-}
+export const audioFiles = [
+  {
+    title: 'Episode 123',
+    mimeType: 'mp3'
+  },
+  {
+    title: 'Episode 123',
+    mimeType: 'ogg'
+  },
+  {
+    title: 'Episode 123',
+    mimeType: 'mp4'
+  }
+]
 
-export const blank = () => ({
-  components: { EpisodeAudioFiles },
-  template: '<episode-audio-files></episode-audio-files>'
-})
+storiesOf('Design Sytem|Molecule/EpisodeAudioFiles', module).add(
+  'default',
+  () => {
+    return {
+      components: { EpisodeAudioFiles },
+      template:
+        '<episode-audio-files :audio-files="audioFiles"></episode-audio-files>',
+      data: () => ({ audioFiles })
+    }
+  }
+)
--- stories/EpisodesTable.story.js
+++ stories/EpisodesTable.story.js
@@ -1,11 +1,29 @@
+import { storiesOf } from '@storybook/vue'
 import EpisodesTable from '../components/EpisodesTable'
 
-export default {
-  title: 'Design Sytem|Molecule/EpisodesTable',
-  component: EpisodesTable
-}
+export const episodes = [
+  {
+    number: 1,
+    title: 'Some title',
+    publishState: 'drafted'
+  },
+  {
+    number: 2,
+    title: 'Some title',
+    publishState: 'drafted'
+  },
+  {
+    number: 3,
+    title: 'Some title',
+    publishState: 'drafted'
+  }
+]
 
-export const blank = () => ({
-  components: { EpisodesTable },
-  template: '<episodes-table></episodes-table>'
+storiesOf('Design Sytem|Molecule/EpisodesTable', module).add('default', () => {
+  return {
+    components: { EpisodesTable },
+    template:
+      '<episodes-table :episodes="episodes" :network="{id: 2}" :podcast="{id: 2}"></episodes-table>',
+    data: () => ({ episodes })
+  }
 })
