b4b67c0c83be367701019f94265f3183b83eca34
Letty <login@letty.io>
2019-11-02 10:57:56 +0100

    Chore: try to add momentjs

--- .storybook/config.js
+++ .storybook/config.js
@@ -2,10 +2,13 @@ import { configure } from '@storybook/vue'
 import Buefy from 'buefy'
 import Vue from 'vue'
 import Vuex from 'vuex'
+var moment = require('moment');
 
 import '!style-loader!css-loader!sass-loader!../assets/css/main.scss'
 
+moment().format();
 Vue.use(Buefy)
 Vue.use(Vuex)
+Vue.use(moment())
 
 configure(require.context('../', true, /\.story\.js$/), module);
