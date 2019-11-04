1cb087c1aad1b29180fc6076033bb9778c8e19f5
Letty <login@letty.io>
2019-11-04 12:12:13 +0100

    Refactor(stories): Add mock data

--- stories/Podcast.story.js
+++ stories/Podcast.story.js
@@ -1,11 +1,47 @@
+import { storiesOf } from '@storybook/vue'
 import Podcast from '../components/Podcast'
 
-export default {
-  title: 'Design Sytem|Molecule/Podcast',
-  component: Podcast
+// TODO: moment js mocken
+// Todo welche edge cases gibts?
+// [ ] keine Episoden
+// [ ] ein paar Episoden
+// [ ] sehr viele Episoden
+
+export const podcast = {
+  image:
+    'http://localhost:9000/radiator/podcast/2/cover_original.jpg?v=63739563914',
+  title: 'Title',
+  subtitle: 'Subtitle'
+  // episodes: [
+  //   {
+  //     number: 1,
+  //     title: 'Some title',
+  //     publishState: 'drafted'
+  //   },
+  //   {
+  //     number: 2,
+  //     title: 'Some title',
+  //     publishState: 'drafted'
+  //   },
+  //   {
+  //     number: 3,
+  //     title: 'Some title',
+  //     publishState: 'drafted'
+  //   }
+  // ]
 }
 
-export const blank = () => ({
-  components: { Podcast },
-  template: '<podcast></podcast>'
+export const network = {
+  id: 1
+}
+
+storiesOf('Design Sytem|Molecule/Podcast', module).add('default', () => {
+  return {
+    components: { Podcast },
+    template: '<podcast :network="network" :podcast="podcast"></podcast>',
+    data: () => ({
+      podcast,
+      network
+    })
+  }
 })
--- stories/Signup.story.js
+++ stories/Signup.story.js
@@ -1,11 +1,21 @@
+import { storiesOf } from '@storybook/vue'
+import { withKnobs } from '@storybook/addon-knobs'
+import Vuex from 'vuex'
 import Signup from '../components/Signup'
 
-export default {
-  title: 'Design Sytem|Molecule/Signup',
-  component: Signup
-}
-
-export const blank = () => ({
-  components: { Signup },
-  template: '<signup></signup>'
-})
+storiesOf('Design Sytem|Molecule/Signup', module)
+  .addDecorator(withKnobs)
+  .add('default', () => {
+    return {
+      components: { Signup },
+      template: '<signup></signup>',
+      store: new Vuex.Store({
+        state: {
+          auth: {
+            isLoggedIn: false
+          }
+        },
+        mutations: {}
+      })
+    }
+  })
