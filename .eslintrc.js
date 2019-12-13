d58458b2bbbbabded0ae73da43153ef95360e773
Letty <login@letty.io>
2019-12-13 18:21:05 +0100

    Refactor: add some rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,6 +19,8 @@ module.exports = {
   ],
   // add your custom rules here
   rules: {
-    'no-console': 0
+    'no-console': 0,
+    'object-shorthand': 0,
+    'unicorn/throw-new-error': 0
   }
 }
